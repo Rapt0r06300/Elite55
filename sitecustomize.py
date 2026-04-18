d78b15225d8f38f643b80fd8e479872e19dc95bc
Rapt0r06300 <bermond.florent06@gmail.com>
2026-04-18 07:39:21 +0200

    Branche aussi le service d’analyse marchandise au démarrage

--- sitecustomize.py
+++ sitecustomize.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 try:
     import app.main as elite_main
+    from app.commodity_intel_service import install_commodity_intel_service_patches
     from app.live_snapshot_backend import install_live_snapshot_backend_patches
     from app.live_snapshot_service import install_live_snapshot_service_patches
     from app.trade_ranking import install_backend_ranking_patches
@@ -11,3 +12,4 @@ else:
     install_backend_ranking_patches(elite_main)
     install_live_snapshot_backend_patches(elite_main)
     install_live_snapshot_service_patches(elite_main)
+    install_commodity_intel_service_patches(elite_main)
