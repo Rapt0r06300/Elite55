e2226df0ada32d611c66060514f86c05deb6c688
Rapt0r06300 <bermond.florent06@gmail.com>
2026-04-18 19:45:04 +0200

    Branche aussi le moteur de suggestions

--- sitecustomize.py
+++ sitecustomize.py
@@ -33,6 +33,7 @@ except Exception as error:
 else:
     _install_step("helpers coeur", "app.core_runtime_service.install_core_runtime_service_patches", elite_main)
     _install_step("correspondance texte", "app.search_match_service.install_search_match_service_patches", elite_main)
+    _install_step("moteur suggestions", "app.suggestion_engine_service.install_suggestion_engine_service_patches", elite_main)
     _install_step("config trader", "app.trade_config_service.install_trade_config_service_patches", elite_main)
     _install_step("moteur de routes", "app.route_engine.install_route_engine_patches", elite_main)
     _install_step("memoire trader", "app.trader_memory_service.install_trader_memory_service_patches", elite_main)
